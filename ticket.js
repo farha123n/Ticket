60777e732e3d1817d75d62b93cf128e29538215f
unknown <you@example.com>
2024-02-20 12:18:01 -0800

    second commit

--- ticket.js
+++ ticket.js
@@ -1,13 +1,43 @@
-function create_element(){
-    let ParentDiv=document.getElementById('information');
+
+function create_element(seatname){
+    let ParentDiv = document.getElementById('information');
     let new_element=document.createElement('div');
-    new_element.id='newDiv';
+    
     new_element.classList.add("flex","justify-between","w-60","my-10")
     ParentDiv.appendChild(new_element);
-    let parentInfonmation=document.getElementById('newDiv');
+
+    let seat_information_element=document.createElement("p");
+    seat_information_element.innerText=seatname;
+    let class_information_element=document.createElement("p");
+    class_information_element.innerText="Business class";
+    let price_inforamtion_element=document.createElement("p");
+    price_inforamtion_element.innerText="550TK"
+    new_element.appendChild(seat_information_element);
+    new_element.appendChild(class_information_element);
+    new_element.appendChild(price_inforamtion_element);
+    
 }
-function information(){
-    let information=document.getElementsByClassName('a');
-    let seatname=information.innerText();
-    create_element(seatname);
+function count(zero){
+    
+}
+
+function information(button){
+    let check=0;
+     let count;
+    if(check==0){
+      count=0;
+
+    }
+    
+    if(count<4){
+        let seatname=button.textContent;
+    console.log(seatname);
+    create_element(seatname)
+    
+    }
+    check++;
+    count++;
+    console.log(count);
+    
+    
 }
