b80dee97d1de761b341c11d213f7f02615ff514a
unknown <you@example.com>
2024-02-20 14:19:40 -0800

    final commit

--- ticket.js
+++ ticket.js
@@ -1,4 +1,6 @@
-
+let count=0;
+let a=[];
+let totalPrice1=0;
 function create_element(seatname){
     let ParentDiv = document.getElementById('information');
     let new_element=document.createElement('div');
@@ -17,27 +19,55 @@ function create_element(seatname){
     new_element.appendChild(price_inforamtion_element);
     
 }
-function count(zero){
-    
+function totalPrice(ticket){
+     totalPrice1=ticket*550;
+    const cost=document.getElementById("total-cost");
+    cost.innerText=totalPrice1;
+    return totalPrice1;
 }
 
-function information(button){
-    let check=0;
-     let count;
-    if(check==0){
-      count=0;
 
-    }
+
+function information(button){
     
-    if(count<4){
+    
+     
+    
+    if((count<4)&&(!a.includes(button.textContent))){
         let seatname=button.textContent;
+        button.classList.add("b");
+        button.classList.remove("bg-slate-200");
+        a.push(seatname);
     console.log(seatname);
     create_element(seatname)
-    
+    count++
     }
-    check++;
-    count++;
-    console.log(count);
-    
-    
+    c=totalPrice(count);
+   return c;
+
+}
+function discount1(){
+    let couponInput=document.getElementById("coupon");
+    const c=couponInput.value;
+    let finalCost;
+    const i=information(this);
+    if(c==="news15")
+    {
+     finalCost=i-(i*15)/100;
+    }
+    else if(c==="couple20")
+    {
+     finalCost=i-(i*20)/100
+    }
+    else{
+     finalCost=i;
+    }
+    finalCostElement=document.getElementById("final-cost");
+    finalCostElement.innerText=finalCost;
+}
+function hide(){
+    const main=document.getElementById("main");
+    main.classList.add("hidden");
+    const header=document.getElementById("header");
+    header.classList.add("hidden");
 }
